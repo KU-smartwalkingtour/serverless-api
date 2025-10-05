4cc02250a3943f8d14a14802185ed149318113f4
gicks04 <gichanyoon@gmail.com>
2025-10-05 21:22:45 +0900

    feat: db 및 users 테이블 변경

--- models/user.js
+++ models/user.js
@@ -10,7 +10,14 @@ const User = sequelize.define('User', {
   password: {
     type: DataTypes.STRING,
     allowNull: false
+  },
+  status: {
+    type: DataTypes.ENUM('active', 'withdrawn'), // 'active':활성상태 또는 'withdrawn':탈퇴상태 값만 허용
+    allowNull: false,                           
+    defaultValue: 'active'                     
   }
+}, {
+  timestamps: true,
 });
 
 module.exports = User;
