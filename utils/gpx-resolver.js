cea426558914ac7302a6b91693c71c30a3ac2460
seungwoo1209 <2002msw@naver.com>
2025-10-27 02:41:53 +0900

    refactor: 기존 gpx-resolver.js에 필요없는 utility 함수 삭제

--- utils/gpx-resolver.js
+++ utils/gpx-resolver.js
@@ -8,10 +8,6 @@ const s3Client = new S3Client({ region: 'ap-northeast-2' });
 const BUCKET_NAME = 'ku-smartwalkingtour-seoultrail-gpxstorage-bucket';
 const GPX_PREFIX = 'gpx_files/';
 
-// 상수
-const EARTH_RADIUS_KM = 6371;
-const DEGREES_TO_RADIANS = Math.PI / 180;
-
 // S3 GetObjectCommand의 Body(Stream)를 문자열로 변환하는 헬퍼 함수
 const streamToString = (stream) =>
   new Promise((resolve, reject) => {
@@ -75,70 +71,6 @@ const getCoordinatesFromGpx = async (gpxFileContent) => {
       resolve(rows);
     });
   });
-};
-
-/**
- * GPX 파일 콘텐츠를 파싱하여 코스 메타데이터를 반환합니다.
- * @param {string} gpxFileContent - GPX 파일의 전체 내용
- * @returns {Promise<object>} 코스 메타데이터 객체
- */
-const getCourseMetadataFromGpx = async (gpxFileContent) => {
-  try {
-    const parser = new xml2js.Parser({ explicitArray: false });
-    const result = await parser.parseStringPromise(gpxFileContent);
-
-    // trk 안의 extensions 접근
-    const extensions = result.gpx.trk.extensions;
-
-    // ogr 네임스페이스는 단순한 key로 들어있음
-    const courseName = extensions['ogr:CONTS_NAME'];
-    const addressOld = extensions['ogr:ADDR_OLD'];
-    const addressNew = extensions['ogr:ADDR_NEW'];
-    const name01 = extensions['ogr:NAME_01'];
-    const value01 = extensions['ogr:VALUE_01'];
-    const name02 = extensions['ogr:NAME_02'];
-    const value02 = extensions['ogr:VALUE_02'];
-    const name03 = extensions['ogr:NAME_03'];
-    const value03 = extensions['ogr:VALUE_03'];
-    const name04 = extensions['ogr:NAME_04'];
-    const value04 = extensions['ogr:VALUE_04'];
-    const name05 = extensions['ogr:NAME_05'];
-    const value05 = extensions['ogr:VALUE_05'];
-    const name06 = extensions['ogr:NAME_06'];
-    const value06 = extensions['ogr:VALUE_06'];
-    const name07 = extensions['ogr:NAME_07'];
-    const value07 = extensions['ogr:VALUE_07'];
-    const name08 = extensions['ogr:NAME_08'];
-    const value08 = extensions['ogr:VALUE_08'];
-    const name09 = extensions['ogr:NAME_09'];
-    const value09 = extensions['ogr:VALUE_09'];
-    const name10 = extensions['ogr:NAME_10'];
-    const value10 = extensions['ogr:VALUE_10'];
-    const name11 = extensions['ogr:NAME_11'];
-    const value11 = extensions['ogr:VALUE_11'];
-
-    return {
-      courseName,
-      addressOld,
-      addressNew,
-      attributes: {
-        [name01]: value01,
-        [name02]: value02,
-        [name03]: value03,
-        [name04]: value04,
-        [name05]: value05,
-        [name06]: value06,
-        [name07]: value07,
-        [name08]: value08,
-        [name09]: value09,
-        [name10]: value10,
-        [name11]: value11,
-      },
-    };
-  } catch (err) {
-    logger.error(`GPX 메타데이터 파싱 실패: ${err.message}`);
-    throw err;
-  }
 };
 
 /**
