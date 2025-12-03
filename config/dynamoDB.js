8f7b309fbb7faa727f176e98812c95ec4dd4dabd
seungwoo1209 <2002msw@naver.com>
2025-12-03 19:33:43 +0900

    fix: removed credential import in dynamodb client

--- config/dynamoDB.js
+++ config/dynamoDB.js
@@ -5,11 +5,11 @@ require('dotenv').config();
 
 // 1. 클라이언트 생성 (연결 정보 설정)
 const client = new DynamoDBClient({
-  region: process.env.AWS_REGION || "ap-northeast-2", // 서울 리전
-  credentials: {
-    accessKeyId: process.env.AWS_ACCESS_KEY_ID,
-    secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
-  },
+  region: process.env.AWS_REGION || "ap-northeast-2" // 서울 리전
+  // credentials: {
+  //   accessKeyId: process.env.AWS_ACCESS_KEY_ID,
+  //   secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
+  // },
 });
 
 // 2. 편의성을 위한 DocumentClient 변환 (Javascript 객체를 바로 넣고 뺄 수 있게 함)
